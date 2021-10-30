1b7caf8c5ab7376aeec6790c4959f3db932977ee
Louie Beren <you@example.com>
2021-10-31 01:10:55 +0800

    Uploaded the local WordPress

--- wp-content/themes/mySampleTheme/functions.php
+++ wp-content/themes/mySampleTheme/functions.php
@@ -3,7 +3,7 @@
 function load_stylesheets()
 {
 
-    wp_register_style('font',get_template_directory_uri(  ). './resources.css', array(), 1, 'all');
+    wp_register_style('font',get_template_directory_uri(  ). '/resources.css', array(), 1, 'all');
     //wp_register_style( $handle:string, $src:string|boolean, $deps:array, $ver:string|boolean|null, $media:string )
     wp_enqueue_style( 'font');
 }
